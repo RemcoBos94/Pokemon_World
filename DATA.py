36accc65f02cd0481381ab9703a146d4b91d800c
Remco <61877733+RemcoBos94@users.noreply.github.com>
2023-02-27 15:08:09 +0100

    Add files via upload

---
+++ DATA.py
@@ -0,0 +1,53 @@
+import Pokedex
+import Pokemon
+import Attack
+import Inventory
+
+
+
+
+def makePokedex():
+    
+    pokedex = Pokedex.Pokedex("pokemonDB")
+    
+
+    pokeDB = "Data/pokedex.csv"
+    fid = open(pokeDB, "r")
+    pokeLijnen = fid.readlines()
+    fid.close()
+
+    for i in range(1, len(pokeLijnen)):
+        pokemonLijn = pokeLijnen[i].strip().split(',')
+        pokemon = Pokemon.Pokemon(pokemonLijn[1], pokemonLijn[2], pokemonLijn[3], pokemonLijn[4], pokemonLijn[5], pokemonLijn[6], pokemonLijn[7])
+        aanvallenDB = "Data/aanvallen.csv"
+        fid = open(aanvallenDB, "r")
+        aanvalLijnen = fid.readlines()
+        fid.close()
+        for j in range(1, len(aanvalLijnen) - 1):
+            aanvalLijn = aanvalLijnen[j].strip().split(',')
+            aanval = Attack.Attack(aanvalLijn[0], aanvalLijn[1], aanvalLijn[2], aanvalLijn[3])
+            if (pokemon.getPrimaryType() == aanval.getType()):
+                pokemon.voegAanvalToe(aanval)
+            if (aanval.getType() == "NORMAL"):
+                pokemon.voegAanvalToe(aanval)
+        pokedex.addPokemon(pokemon)
+        print("pokecheck")
+        
+    return pokedex
+
+    #aanval_per_type_db = dict()
+    # Bestanden uitlezen kunnen we doen met behulp van de open/close functies.
+    
+
+def createInventoryMainPlayer():
+    """
+    Initialiseerd de inventory
+    Returns
+    -------
+    dict:
+        inventory in dictionary met als keys pokeball, masterball, health potions en raspberries
+    """
+    inventory = Inventory.Inventory(5,5,5,5,0)
+    return inventory
+
+    
---
+++ Pokemon.py
@@ -0,0 +1,69 @@
+class Pokemon:
+    """
+    Klasse die pokemon voorstelt.
+    """
+    def __init__(self, name, primaryType, secondarytype, hp, pp, defense, speed):
+        """
+        Maakt een nieuwe pokemon aan.
+        """
+        
+        self._id = 0
+        self._name = name
+        self._primaryType = primaryType
+        self._secondaryType = secondarytype
+        self._hp = hp
+        self._pp = pp
+        self._maxhp = hp
+        self._maxpp = pp
+        self._defense = defense
+        self._speed = speed
+        self._aanvallen = []
+
+
+    def getName(self):
+        return self._name
+
+    def getPrimaryType(self):
+        return self._primaryType
+
+    def getHP(self):
+        return self._hp
+
+    def getPP(self):
+        return self._pp
+
+    def getMaxHP(self):
+        return self._maxhp
+
+    def getMaxPP(self):
+        return self._maxpp
+
+    def getDefense(self):
+        return self._defense
+
+    def getSpeed(self):
+        return self._speed
+
+    def getAttacks(self):
+        return self._aanvallen
+
+    def incrementID(self):
+        self._id += 1
+
+    def setHP(self, HP):
+        if(HP > int(self._maxhp)):
+            self._hp = self._maxhp
+        else:
+            self._hp = HP
+
+    def setPP(self, PP):
+        if(PP > int(self._maxpp)):
+            self._pp = self._maxpp
+        else:
+            self._pp = PP
+
+
+    def voegAanvalToe(self, aanval):
+        self._aanvallen.append(aanval)
+
+    
